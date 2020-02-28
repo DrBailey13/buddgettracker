dcaa554419e81f63bfb597f0e076620f069e8f4c
DeVante Bailey <devantebailey1322@gmail.com>
2020-02-28 10:43:51 -0500

    added route and updated server.js

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
 
 const PORT = 3000;
 
 const app = express();
 
-app.use(logger("dev"));
-
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -20,7 +15,8 @@ mongoose.connect("mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
-// routes here
+// routes
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
