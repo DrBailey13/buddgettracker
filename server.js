769122da77ed98093da25d360ed97e71da0122fe
DeVante Bailey <devantebailey1322@gmail.com>
2020-02-29 12:17:46 -0500

    added process.env to port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
